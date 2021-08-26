6a4624db931a1f97f08db2a141237ebb7b60d73e
killerpie <52218913+killerpie@users.noreply.github.com>
2021-08-26 13:36:56 -0400

    Update 3_GreedyMethod.py
    added Fractional Knapsack Greedy Method

--- 3_GreedyMethod.py
+++ 3_GreedyMethod.py
@@ -14,35 +14,29 @@ n = number of objects
 w = weight capacity
 '''
 
-#Greedy Method
+# Fractional Knapsack Greedy Method
 class KnapsackPackage(object):
   """ Knapsack package data class """
   def __init__(self, weight, value):
     self.weight = weight
     self.value = value
-    self.cost = value/weight
-  
+    self.pervalue = round(value/weight,3)
+
   def __lt__(self, other):
-    return self.cost < other.cost
-  
-if __name__ == "__main__":
-  W = [15, 10, 2, 4]
-  V = [30, 25, 2, 6]
-  M = 37
-  n = 4
-  
-  proc = FractionalKnapsack()
-  proc.knapsackGreProc(W, V, M, n) 
-  
-class FractionalKnapsack(object):
-  def __init__(self):
-    
-  def knapsackGreProc(self, W, V, M, n):
+    #less than operator(<). Return True or False. when sorted, it sorts by pervalue
+    return self.pervalue < other.pervalue
+
+def knapsackGreedy(W, V, M, n):
     packs = []
+    print ("{:<8} {:<8} {:<8}".format("value", "weight", "pervalue"))
     for i in range(n):
-      packs.append(KnapsackPckage(W[i], V[i]))
+      packs.append(KnapsackPackage(W[i], V[i]))
+      #print("Value", packs[i].value,"Weight", packs[i].weight,"per Value", packs[i].pervalue)
+      print ("{:<8} {:<8} {:<8}".format(packs[i].value, packs[i].weight, packs[i].pervalue))
       
-    packs.sort(reverse = True)
+    packs.sort(reverse = True) #Sort by pervalue?
+
+
     
     remain = M
     result = 0
@@ -50,18 +44,36 @@ class FractionalKnapsack(object):
     i = 0
     stopProc = False
     
- while  (stopProc != True):
-  if (packs[i].weight <= remain):
-    remain -= packs[i].weight
-    result += packs[i].value
+    while(stopProc != True):
+        while (packs[i].weight >= 1 and remain != 0):
+            remain -= 1
+            result += packs[i].pervalue
+            packs[i].weight -= 1
+            print("Pack ", i, " - Weight ", packs[i].weight, " - Value ", packs[i].value, " - perValue ", packs[i].pervalue)
+        i += 1
 
- print("Pack ", i, " - Weight ", packs[i].weight, " - Value ", packs[i].value)
-  if (packs[i].weight > remain):
-    i += 1
-  if (i == n):
-    stopProc = True           
-         
- print("Max Value:\t", result)   
+        if (i == n):
+            stopProc = True
+
+    print("Max Value:\t", result)
+
+
+
+'''
+W = [15, 10, 2, 4]
+V = [30, 25, 2, 6]
+M = 37
+n = 4
+'''
+
+W = [2, 3, 5, 7, 1, 4, 1]
+V = [10, 5, 15, 7, 6, 18, 3]
+M = 15
+n = 7
+
+
+knapsackGreedy(W, V, M, n)
+
 
 
 
